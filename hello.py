8438ddc07ee17e839f84adea662755095af40b92
DESKTOP-R4IFPD3\hp <i>
2022-09-29 15:29:25 +0100

    2nd commit 29/9/2022

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 print('HELLO PYTHON, IS A NEW BEGINNING')
 a = 10 #a is the variable and 10 is the object
 print(a)
+b = 10 * 2
+c = 15 + 15
+d = 'hello ' + 'good ' + 'goodmorning'
+e = [2,3,4] + [5,6,7]
+
+print(b)
+print(c)
+print(d)
+print(e)
