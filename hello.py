a7d03420d5c653ef9a02ade0f2f6697212e5586c
DESKTOP-R4IFPD3\hp <i>
2022-09-30 04:28:53 +0100

    .

--- hello.py
+++ hello.py
@@ -10,3 +10,34 @@ print(b)
 print(c)
 print(d)
 print(e)
+
+f = 'come here ' * 5;
+print(f)
+
+#--------------------------------------------------------------------------------------------------
+if 15 < 25:
+    print('yes this is true')
+else:
+    print('no not true')
+ 
+#----     
+DOB = 9  
+
+pass
+if DOB < 10:
+    print('you are a child, go away')
+    
+elif DOB > 18:
+    print('you can vote')
+else:  
+    print('wait for the court to decide')
+    
+    
+    
+#------------------------------- WHILE LOOP
+
+number = 0
+while number < 10:
+    print(f"Number is {number}!")  #Then by using f-string, expression specified by {} will be replaced with it's value.
+    number = number + 1
+    
