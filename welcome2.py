ec1c2357ce5f0a8cabc80198efb8eb874b1da31f
DESKTOP-R4IFPD3\hp <i>
2022-10-03 11:15:17 +0100

    .

--- welcome2.py
+++ welcome2.py
@@ -1,1 +1,19 @@
 #this is week2
+
+
+#functions
+
+#note -> Note the difference between parameters and arguments: 
+# #Function parameters are the names listed in the function's definition(placeholders). 
+#Function arguments are the real values passed to the function
+
+
+
+def print_name(name, age, country):
+    print(f'{name}, {age}, {country}')
+    
+    
+#A positional argument means its position matters in a function call. A keyword argument is a function argument with a name label.
+
+print_name('tom', 19, 'Canada')   #this is called positional arguments
+print_name(country='nigeria', age=12, name='jerry')   #this is called keyword argument
