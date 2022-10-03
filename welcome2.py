cb9f2eb64a60f32e07654cb950243e7294359dd9
DESKTOP-R4IFPD3\hp <i>
2022-10-03 11:26:09 +0100

    .

--- welcome2.py
+++ welcome2.py
@@ -17,3 +17,12 @@ def print_name(name, age, country):
 
 print_name('tom', 19, 'Canada')   #this is called positional arguments
 print_name(country='nigeria', age=12, name='jerry')   #this is called keyword argument
+
+
+def print_name(name, country, age=25):
+    print(f'{name}, {age}, {country}')
+    
+
+# you can add default values to function parameters
+print_name(country='nigeria', name='merry')
+print_name('tom', 'Canada') 
